e8f689cc81732f06c45dd1bec97b25031f0a1a37
Guillaume Aveline <guillaume@codr.fr>
2017-12-25 00:53:27 +0100

    simplify offset and x calculation & fix offset on new box

--- sketch.js
+++ sketch.js
@@ -8,21 +8,21 @@ let boxSize = {
 }
 
 let currentIndex = 0;
-let stackSize = 32;
+let lastIndex = 1;
+let stackSize = 6;
 let stack = [];
 let stackHeight = 0;
 
 class Box {
     constructor(w, h, d) {
-        this.ox = 0;
-        this.oz = 0;
         this.width = w;
         this.height = h;
         this.depth = d;
-        this.a = 0;
         this.x = 0;
         this.y = 0;
         this.z = 0;
+        this.ox = 0;
+        this.oz = 0;
     }
 
     draw() {
@@ -47,23 +47,24 @@ function setup() {
 function mousePressed() {
     side = (side === "x") ? "z" : "x";
 
-    let prevIndex = currentIndex;
+    prevIndex = currentIndex;
     currentIndex--;
 
     if(currentIndex < 0) {
         currentIndex = stackSize-1;
     }
 
-    stack[currentIndex].a = 0;
     stack[currentIndex].x = stack[prevIndex].x + stack[prevIndex].ox;
     stack[currentIndex].y = boxSize.h;
     stack[currentIndex].z = stack[prevIndex].z + stack[prevIndex].oz;
+    stack[currentIndex].ox = 0;
+    stack[currentIndex].oz = 0;
 
     for (let i = 0; i < stackSize; i++) {
         stack[i].y -= boxSize.h;
     }
 
-
+    a=0;
 }
 
 function draw() {
@@ -75,16 +76,18 @@ function draw() {
 
     for (let i = 0; i < stackSize; i++) {
         push();
+
         if (i === currentIndex) {
             if (side === "x") {
-                stack[i].ox = cos(stack[i].a) * stack[i].width;
+                stack[i].ox = cos(a) * stack[i].width;
             } else {
-                stack[i].oz = cos(stack[i].a) * stack[i].depth;
+                stack[i].oz = cos(a) * stack[i].depth;
             }
-            stack[i].a += 0.06;
         }
 
         stack[i].draw();
         pop();
     }
+
+    a += 0.05;
 }
