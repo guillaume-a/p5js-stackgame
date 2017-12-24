c2dc86c45de74ee41683388f57c2282fc0b54e31
Guillaume Aveline <guillaume@codr.fr>
2017-12-24 16:09:44 +0100

    3D iso and moving cube

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
+let a = 0;
+let height = 50;
 
 function setup() {
-    createCanvas(600, 600);
-    background(150);
+    createCanvas(600, 600, WEBGL);
+    ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 500);
+
 }
 
 function draw() {
+
+    rotateX(-QUARTER_PI);
+    rotateY(atan(1));
+
+    background(50);
+    normalMaterial();
+
+    translate(cos(a)*70, 0, 0);
+    box(50);
+
+    a+=0.1;
 
 }
