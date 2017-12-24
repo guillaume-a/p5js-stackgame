0d5031a2c60ec1d54b21f6232a919aad742189a9
Guillaume Aveline <guillaume@codr.fr>
2017-12-24 16:26:59 +0100

    create box class

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
 let a = 0;
-let boxWidth = 200;
-let boxHeight = 40;
-let boxDepth = 200;
-
 let side = "x";
+let currentBox;
+
+let boxSize = {
+    w: 200,
+    h: 40,
+    d: 200
+}
+
+class Box {
+    constructor(w, h, d) {
+        this.width = w;
+        this.height = h;
+        this.depth = d;
+    }
+
+    draw() {
+        box(this.width, this.height, this.depth);
+    }
+}
 
 function setup() {
     createCanvas(600, 600, WEBGL);
     ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 500);
+
+    currentBox = new Box(boxSize.w, boxSize.h, boxSize.d);
 }
 
 function mousePressed() {
@@ -22,12 +39,12 @@ function draw() {
     normalMaterial();
 
     if(side === "x") {
-        translate(cos(a)*boxWidth, 0, 0);
+        translate(cos(a)*boxSize.w, 0, 0);
     } else {
-        translate(0, 0, cos(a)*boxDepth);
+        translate(0, 0, cos(a)*boxSize.d);
     }
 
-    box(boxWidth, boxHeight, boxDepth);
+    currentBox.draw();
 
     a+=0.06;
 
