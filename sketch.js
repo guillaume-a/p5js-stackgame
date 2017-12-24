d7689e9582fa0e78f53a7b38fee53a805d59b576
Guillaume Aveline <guillaume@codr.fr>
2017-12-24 16:59:32 +0100

    move drawing logic in box

--- sketch.js
+++ sketch.js
@@ -7,20 +7,24 @@ let boxSize = {
     d: 200
 }
 
-let currentBox = 0;
-let stackSize = 10;
+let currentIndex = 0;
+let stackSize = 2;
 let stack = [];
+let stackHeight = 0;
 
 class Box {
     constructor(w, h, d) {
         this.width = w;
         this.height = h;
         this.depth = d;
+        this.a = 0;
         this.x = 0;
+        this.y = 0;
         this.z = 0;
     }
 
     draw() {
+        translate(this.x, this.y, this.z);
         box(this.width, this.height, this.depth);
     }
 }
@@ -29,16 +33,23 @@ function setup() {
     createCanvas(600, 600, WEBGL);
     ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 500);
 
+    let currentBox;
+
     for (let i = 0; i < stackSize; i++) {
-        stack[i] = new Box(boxSize.w, boxSize.h, boxSize.d);
+        currentBox = new Box(boxSize.w, boxSize.h, boxSize.d);
+        currentBox.y = -i * boxSize.h;
+
+        stack[i] = currentBox;
+        stackHeight -= boxSize.h;
     }
 }
 
 function mousePressed() {
     side = (side === "x") ? "z" : "x";
-    currentBox--;
-    if(currentBox < 0) {
-        currentBox = stackSize-1;
+    currentIndex--;
+
+    if(currentIndex < 0) {
+        currentIndex = stackSize-1;
     }
 }
 
@@ -50,21 +61,17 @@ function draw() {
     normalMaterial();
 
     for (let i = 0; i < stackSize; i++) {
-        translate(0, -boxSize.h, 0);
-
         push();
-        if (i === currentBox) {
+        if (i === currentIndex) {
             if (side === "x") {
-                translate(cos(a) * boxSize.w, 0, 0);
+                stack[i].x = cos(stack[i].a) * stack[i].width;
             } else {
-                translate(0, 0, cos(a) * boxSize.d);
+                stack[i].z = cos(stack[i].a) * stack[i].depth;
             }
+            stack[i].a += 0.06;
         }
+        
         stack[i].draw();
-
         pop();
     }
-
-    a += 0.06;
-
 }
